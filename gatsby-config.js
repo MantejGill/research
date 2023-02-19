4f04c0563fdf5c2ffdc702fbb5f2f3e2fbc9a98b
Mantej Singh Gill <mantej.gill@gmail.com>
2023-02-19 23:55:49 +0530

    Removed trailing slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Mantej Singh Gill',
     description:
       'Mantej Singh Gill is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://mantejgill.github.io/research/', // No trailing slash allowed!
+    siteUrl: 'https://mantejgill.github.io/research', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
