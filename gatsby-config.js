4fc481bd5e54950bfa363146a274189aa24e8cac
Mantej Singh Gill <mantej.gill@gmail.com>
2023-02-24 13:32:01 +0530

    Rebuilt with new URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,8 @@ module.exports = {
     title: 'Mantej Singh Gill',
     description:
       'Mantej Singh Gill is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://mantejgill.github.io/research', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    siteUrl: 'https://www.mantejgill.com', // No trailing slash allowed!
+    image: '/demo.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
   plugins: [
